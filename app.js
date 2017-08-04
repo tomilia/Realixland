75b708a5e6088c7ca2f41ce711eeb0f9a48365ff
tomilia <tommylee@Tommys-MacBook-Air.local>
2017-08-05 01:50:25 +0800

    app.js and router connect to detail page

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var cart = require('./routes/cart');
 var search = require('./routes/search');
+
 var id=require('./routes/id');
 var mongoose=require('mongoose');
 
--- routes/id.js
+++ routes/id.js
@@ -1,10 +1,22 @@
 var express = require('express');
+var app=express();
 var router = express.Router();
 var CompData=require ('../models/companymodel');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('id',{title:"ppp"});
+router.get('/:id', function(req, res, next) {
+  var xd=req.params.id;
+  CompData.find({_id:req.params.id}).exec(function (err, docs) {
+      if (err) {
+          throw cb(err);
+      }
+res.render("id",{data:docs});
+      // do some stuff with docs & pass or directly pass it
+      console.log(docs[0].company);
+  });
+  console.log('Request Id:', req.params.id);
+
 });
+
 
 module.exports = router;
